8442f9bcc224817bccb1423577d9c6d77caeba29
Oooom <omishah999@gmail.com>
2022-03-17 20:16:03 +0530

    added point cloud viewer

--- optics_demo.js
+++ optics_demo.js
@@ -13,6 +13,19 @@ var data = require('./pc.json')
 var dataset = data.map((obj) => [obj.x, obj.y, obj.z] )
 
 // parameters: 2 - neighborhood radius, 2 - number of points in neighborhood to form a cluster
-var clusters = optics.run(dataset, 2, 4);
+var clusters = optics.run(dataset, 0.5, 4);
 var plot = optics.getReachabilityPlot();
-console.log(clusters);
+
+var op_data = []
+
+for(var cluster of clusters){
+    var clone = []
+
+    for(var idx of cluster){
+        clone.push(dataset[idx])
+    }
+
+    op_data.push(clone)
+}
+
+console.log("var clusteredPointCloud = " + JSON.stringify(op_data, undefined, 4))
