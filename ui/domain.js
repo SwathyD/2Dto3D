dae2b865b3c647f88e60cbcc07aea2d7b9f7c550
Oooom <omishah999@gmail.com>
2022-04-23 20:02:29 +0530

    final changes

---
+++ ui/domain.js
@@ -0,0 +1,5 @@
+var ngrok_add = "8251-35-185-62-132.ngrok.io"
+var domain = "http://" + ngrok_add;
+var web_ip = "192.168.43.191"
+var static_domain = domain + "/static"
+var https_static_domain = "https://" + ngrok_add + "/static"
