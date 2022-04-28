a1c80e2055c796bc1e44b6fcc9ee0410d28c0db1
swathynair <swathy.dhanesh00@gmail.com>
2022-04-28 09:19:29 +0530

    Video upload

--- ui/domain.js
+++ ui/domain.js
@@ -1,5 +1,5 @@
-var ngrok_add = "8251-35-185-62-132.ngrok.io"
+var ngrok_add = "f80c-34-132-86-170.ngrok.io"
 var domain = "http://" + ngrok_add;
-var web_ip = "192.168.43.191"
+var web_ip = "192.168.141.1"
 var static_domain = domain + "/static"
 var https_static_domain = "https://" + ngrok_add + "/static"
