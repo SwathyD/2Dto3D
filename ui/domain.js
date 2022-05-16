91d2d51b26300c604e2a3b366c8681c638ed5541
Oooom <omishah999@gmail.com>
2022-05-16 12:12:12 +0530

    changes

--- ui/domain.js
+++ ui/domain.js
@@ -1,5 +1,5 @@
-var ngrok_add = "f80c-34-132-86-170.ngrok.io"
+var ngrok_add = "87a9-34-145-196-173.ngrok.io"
 var domain = "http://" + ngrok_add;
-var web_ip = "192.168.141.1"
+var web_ip = "192.168.1.6"
 var static_domain = domain + "/static"
 var https_static_domain = "https://" + ngrok_add + "/static"
